bac07f3ecdf55082e19100716e9af81646de9192
Neroxd10 <neroxd10@gmail.com>
2024-04-26 23:59:55 +0800

    Play Integrity APK

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE := Lineage
+LOCAL_SRC_FILES := Lineage.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
